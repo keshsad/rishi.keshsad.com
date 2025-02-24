efe015c38d23a81f51af0eaa03404d914ec89365
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-02-24 12:44:46 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-templ-tailwind-htmx-chi-air-make-0.0.1
+
+go 1.23.4
+
+require (
+	github.com/a-h/templ v0.3.833
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/server/routes.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"go-templ-tailwind-htmx-chi-air-make-0.0.1/cmd/web"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	r.Get("/", s.HelloWorldHandler)
+
+	fileServer := http.FileServer(http.FS(web.Files))
+	r.Handle("/assets/*", fileServer)
+	r.Get("/web", templ.Handler(web.HelloForm()).ServeHTTP)
+	r.Post("/hello", web.HelloWebHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
